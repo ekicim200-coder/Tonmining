a4f33c458e043713fe6d5a211e47a451a6bc74f9
ekicim200-coder <ekicim200@gmail.com>
2026-01-28 23:37:08 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // firebase-config.js
 
-// Firebase Kütüphanelerini İçe Aktar (Modüler yapı yerine CDN global kullanımı)
+// Firebase Kütüphanelerini İçe Aktar
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// --- BURAYI KENDİ BİLGİLERİNLE DOLDUR ---
+// --- SENİN BİLGİLERİN (DÜZELTİLMİŞ HALİ) ---
 const firebaseConfig = {
     apiKey: "AIzaSyDXwByb4qNJeH5F9pYA8ry-zYcBhdzKsOo",
     authDomain: "tonm-77373.firebaseapp.com",
@@ -12,7 +12,7 @@ const firebaseConfig = {
     storageBucket: "tonm-77373.firebasestorage.app",
     messagingSenderId: "507031118335",
     appId: "1:507031118335:web:1d209e303dca154ec487ca",
-    measurementId: "G-5EV1T50VK8",
+    measurementId: "G-5EV1T50VK8"
 };
 
 // Firebase'i Başlat
@@ -25,7 +25,4 @@ window.firebaseSetDoc = setDoc;
 window.firebaseGetDoc = getDoc;
 window.firebaseDoc = doc;
 
-
 console.log("Firebase Bağlantısı Hazır!");
-
-
