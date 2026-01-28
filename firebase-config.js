3b74944d3def17644221ffd4cb0948e622de6805
ekicim200-coder <ekicim200@gmail.com>
2026-01-28 23:31:18 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,8 +11,8 @@ const firebaseConfig = {
     projectId: "tonm-77373",
     storageBucket: "tonm-77373.firebasestorage.app",
     messagingSenderId: "507031118335",
-    appId: "1:507031118335:web:1d209e303dca154ec487ca"
-    measurementId: "G-5EV1T50VK8
+    appId: "1:507031118335:web:1d209e303dca154ec487ca",
+    measurementId: "G-5EV1T50VK8,
 };
 
 // Firebase'i Başlat
@@ -27,3 +27,4 @@ window.firebaseDoc = doc;
 
 
 console.log("Firebase Bağlantısı Hazır!");
+
