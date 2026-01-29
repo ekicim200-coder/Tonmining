bfdc15e757a67113d8ca81d7588cd8bf6bf3508e
ekicim200-coder <ekicim200@gmail.com>
2026-01-29 04:59:28 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,22 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, doc, setDoc, getDoc, collection, addDoc, query, orderBy, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDXwByb4qNJeH5F9pYA8ry-zYcBhdzKsOo",
     authDomain: "tonm-77373.firebaseapp.com",
-    projectId: "tonm-77373", // <--- ID KESİN DOĞRU
+    projectId: "tonm-77373",
     storageBucket: "tonm-77373.firebasestorage.app",
     messagingSenderId: "507031118335",
-    appId: "1:507031118335:web:1d209e303dca154ec487ca",
-    measurementId: "G-5EV1T50VK8"
+    appId: "1:507031118335:web:1d209e303dca154ec487ca"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-console.log("🔥 Firebase Config Yüklendi ve Hazır!");
-
-// BURASI ÇOK ÖNEMLİ: Fonksiyonları "Export" ediyoruz
-export { db, doc, setDoc, getDoc, collection, addDoc, query, orderBy, getDocs };
+// Bağlantıyı Başlat
+try {
+    firebase.initializeApp(firebaseConfig);
+    
+    // Veritabanını "window" içine atıyoruz ki app.js görebilsin
+    window.db = firebase.firestore();
+    
+    console.log("🔥 Config Başarıyla Yüklendi.");
+} catch (error) {
+    alert("Config Hatası: " + error.message);
+}
