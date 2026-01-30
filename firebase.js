f2332561563a0cf72276f44b31769cd0ab5a77fd
ekicim200-coder <ekicim200@gmail.com>
2026-01-30 04:45:02 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 // firebase-config.js
 
-// Firebase kütüphanelerini CDN'den çekiyoruz
+// 1. Kütüphaneleri CDN'den çekiyoruz
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
+// 2. Senin Ayarların
 const firebaseConfig = {
   apiKey: "AIzaSyDXwByb4qNJeH5F9pYA8ry-zYcBhdzKsOo",
   authDomain: "tonm-77373.firebaseapp.com",
@@ -15,10 +16,10 @@ const firebaseConfig = {
   measurementId: "G-5EV1T50VK8"
 };
 
-// Başlatma
+// 3. Başlatma
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-// Diğer dosyalara özellikleri aç
+// 4. Hepsini dışarı aktar (app.js bunları kullanacak)
 export { db, analytics, doc, getDoc, setDoc };
