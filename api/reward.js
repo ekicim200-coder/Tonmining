7490b4f72048c0873befc1853e7711dfab6dcaa2
ekicim200-coder <ekicim200@gmail.com>
2026-02-10 19:19:53 +0300

    Delete api directory

--- api/reward.js
+++
@@ -1,6 +0,0 @@
-// Dosya: api/reward.js
-export default function handler(req, res) {
-    const { userid } = req.query;
-    console.log(`Ödül verildi: ${userid}`);
-    return res.status(200).send('OK');
-}
